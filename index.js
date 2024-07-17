29761877c5d39b5605e56bbd680a91f053f3c6e9
fxhmyz <alifahmy99111@gmail.com>
2024-07-17 14:58:30 +0300

    first initial commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+// Function to generate thank you cards
+function writeCards(names, event) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages;
+  }
+  
+  // Function to count down from a given number to zero
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  // Example calls:
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+  countDown(10);
+  
